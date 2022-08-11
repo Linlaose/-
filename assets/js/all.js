7cc2540f4163b9b1c34ace493bd514b457218ba3
您的姓名 <98501671+Linlaose@users.noreply.github.com>
2022-08-11 23:34:08 +0800

    Update 2022-08-11T15:34:04.446Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,7 +1,15 @@
 "use strict";
 
-console.log('Hello!');
-$(document).ready(function () {
-  console.log('HesSchool Hello!');
-});
+var logo = document.querySelector('.logo');
+var hamburger = document.querySelector('.hamburger');
+var toggleMenu = document.querySelector('.menu-list');
+
+if (logo) {
+  hamburger.addEventListener('click', function (e) {
+    /* 點到 checkbox */
+    toggleMenu.classList.toggle('menu-show');
+  });
+}
+
+;
 //# sourceMappingURL=all.js.map
