718fa694aab84927522d0ad322ee36975cdc52f2
您的姓名 <98501671+Linlaose@users.noreply.github.com>
2022-08-25 19:58:29 +0800

    更新(lauout.ejs)：將 logo 特效加上斷點設計，768px 以下不出現 logo 特效

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,8 @@ module.exports = {
       'md': '768px',
       // => @media (min-width: 768px) { ... }
 
-      'lg': '1024px',
-      // => @media (min-width: 1024px) { ... }
+      'lg': '992px',
+      // => @media (min-width: 992px) { ... }
 
       'xl': '1280px',
       // => @media (min-width: 1280px) { ... }
