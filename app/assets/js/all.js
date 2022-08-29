97ec7f00a8c6b79504d13a577fef7b8877877257
您的姓名 <98501671+Linlaose@users.noreply.github.com>
2022-08-29 15:05:17 +0800

    新增(login.html)：完成 90%，剩下 js 會員功能以及 mask 動畫連貫問題

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -87,3 +87,56 @@ const certificateSwiper = new Swiper('.certificate-swiper', {
 
 // Animate on scroll
 AOS.init();
+
+//! 自寫邏輯 (登入和註冊動畫)
+
+/* 登入按鈕、區塊、區塊文字宣告 */
+const loginButton = document.querySelector('#login-mask-signIn');
+const loginMask = document.querySelector('#login-mask');
+const loginMaskText = document.querySelector('#login-mask-text-title')
+const loginSubmit = document.querySelector('#loginSubmit') //* sign in
+
+/* 註冊按鈕、區塊、區塊文字宣告 */
+const signUpButton = document.querySelector('#sign-up-mask-signUp')
+const signUpMask = document.querySelector('#sign-up-mask');
+const signUpText = document.querySelector('#sign-up-mask-text-title')
+const signUpSubmit = document.querySelector('#signUpSubmit')
+
+/* 電腦以上才 show 出遮罩動畫 */
+if (window.innerWidth >= 992) {
+  loginButton.addEventListener('click', (e) => {
+    // 登入的遮罩按鈕觸發後
+
+    // 遮罩移動，文字反向移動
+    loginMask.classList.add('translate-x-full', 'opacity-0')
+    loginMaskText.classList.add('-translate-x-full')
+
+    loginMask.classList.add('-z-10')
+    signUpMask.classList.remove('-z-10')
+
+    // 避免按不到註冊按鈕，將其移除
+    loginMask.classList.remove('z-10')
+
+    // 讓註冊遮罩回歸
+    signUpMask.classList.remove('-translate-x-full', 'opacity-0')
+    signUpText.classList.remove('translate-x-full')
+  });
+
+  signUpButton.addEventListener('click', (e) => {
+    // 註冊的遮罩按鈕觸發後
+
+    // 遮罩移動，文字反向移動
+    signUpMask.classList.add('-translate-x-full', 'opacity-0')
+    signUpText.classList.add('translate-x-full')
+
+    signUpMask.classList.add('-z-10')
+
+    // 避免註冊按鈕觸發後，無法再次觸發登入按鈕
+    loginMask.classList.add('z-10')
+    loginMask.classList.remove('-z-10')
+
+    // 讓登入遮罩回歸
+    loginMask.classList.remove('opacity-0', 'translate-x-full')
+    loginMaskText.classList.remove('-translate-x-full')
+  });
+}
