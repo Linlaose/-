caa96238e7a201db9344e73a5a224dfd27fa0a3d
您的姓名 <98501671+Linlaose@users.noreply.github.com>
2022-09-10 20:50:56 +0800

    Update 2022-09-10T12:50:51.511Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -2,12 +2,16 @@
 
 var logo = document.querySelector('.logo');
 var hamburger = document.querySelector('.hamburger');
-var toggleMenu = document.querySelector('.menu-list'); // if (logo) {
-//   hamburger.addEventListener('click', (e) => { /* 點到 checkbox */
-//     toggleMenu.classList.toggle('menu-show');
-//   })
-// };
-// foods-swiper
+var toggleMenu = document.querySelector('.menu-list');
+
+if (logo) {
+  hamburger.addEventListener('click', function (e) {
+    /* 點到 checkbox */
+    toggleMenu.classList.toggle('menu-show');
+  });
+}
+
+; // foods-swiper
 
 var swiper = new Swiper('.foods-swiper', {
   // Optional parameters
